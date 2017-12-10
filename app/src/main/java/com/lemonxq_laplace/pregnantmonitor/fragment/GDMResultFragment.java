9a2f13b3b750673d43d013ab8d3cc0aa58cfd66d
Lemon-XQ <13085758829@163.com>
2017-12-10 20:24:11 +0800

    身高体重年龄空腹血糖输入改为滑动标尺模式，优化UI

--- app/src/main/java/com/lemonxq_laplace/pregnantmonitor/fragment/GDMResultFragment.java
+++ app/src/main/java/com/lemonxq_laplace/pregnantmonitor/fragment/GDMResultFragment.java
@@ -1,7 +1,6 @@
 package com.lemonxq_laplace.pregnantmonitor.fragment;
 
 import android.content.res.Resources;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -22,6 +21,7 @@ public class GDMResultFragment extends Fragment {
 
     private View view;
     private DountChartView dountChartView;
+    private TextView msgText;
     private TextView resultText;
     private float GDM_Prob;
 
@@ -37,27 +37,34 @@ public class GDMResultFragment extends Fragment {
     private void Init(){
         resultText = view.findViewById(R.id.GDMResult);
         dountChartView = view.findViewById(R.id.dountChart);
+        msgText = view.findViewById(R.id.AnalyseRes);
         Resources resources = this.getActivity().getApplicationContext().getResources();
 
         // 计算、设置环形图占比，更改结果显示
         float GDM_per = GDM_Prob * 100;
-//      dountChartView.setPercentage(GDM_per,100-GDM_per);
+        dountChartView.setPercentage(GDM_per,100-GDM_per);
 
-        // 患病
-        if(Float.compare(GDM_Prob, 1.0f) == 0){
-            resultText.setTextColor(Color.RED);
+        // 患病（大于等于0.5）
+        if(Float.compare(GDM_Prob, 0.5f) >= 0){
+            // 设置检测结果文本、字体颜色
+            msgText.setText(resources.getString(R.string.GDM_msg));
+            msgText.setTextColor(resources.getColor(R.color.red));
+            // 设置结果方框样式[文本、红色字体，红色框]
+            resultText.setTextColor(resources.getColor(R.color.red));
             resultText.setText("妊娠期糖尿病");
             Drawable drawable = resources.getDrawable(R.drawable.redoutlinerect);
             resultText.setBackground(drawable);
-            dountChartView.setPercentage(75,25);
         }
         // 不患病
-        else if(Float.compare(GDM_Prob, -1.0f) == 0){
-            resultText.setTextColor(Color.GREEN);
+        else {
+            // 设置检测结果文本、字体颜色
+            msgText.setText(resources.getString(R.string.normal_msg));
+            msgText.setTextColor(resources.getColor(R.color.green));
+            // 设置结果方框样式[文本、绿色字体，绿色框]
+            resultText.setTextColor(resources.getColor(R.color.green));
             resultText.setText("正常");
             Drawable drawable = resources.getDrawable(R.drawable.greenoutlinerect);
             resultText.setBackground(drawable);
-            dountChartView.setPercentage(25,75);
         }
     }
 
